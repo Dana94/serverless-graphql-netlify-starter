77c91c79b2f3c08037e509a90c84b4fab75a6741
Dana <dana.ottaviani@gmail.com>
2020-03-24 10:02:37 -0400

    feat: add 2 quotes and their resolvers

--- src/server.js
+++ src/server.js
@@ -3,6 +3,32 @@ const ApolloServer = require('apollo-server').ApolloServer
 const ApolloServerLambda = require('apollo-server-lambda').ApolloServer
 const { gql } = require('apollo-server-lambda');
 
+
+const authors = [
+  {
+    id: 1,
+    author: 'Eleanor Roosevelt'
+  },
+  {
+    id: 2,
+    quote: 'First we eat, then we do everything else.',
+    author: 'M.F.K. Fisher'
+  },
+]
+
+const quotes = [
+  {
+    id: 1,
+    authorId: 1,
+    quote: 'A woman is like a Tea Bag. You never know how strong she is until she gets into hot water.',
+  },
+  {
+    id: 2,
+    authorId: 2,
+    quote: 'First we eat, then we do everything else.',
+  },
+];
+
 const typeDefs = gql`
   type Query {
     hello: String
@@ -11,11 +37,21 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    hello: () => "Hi! Love from @stemmlerjs 🤠."
+    hello: () => "Hi! Love from @stemmlerjs 🤠.",
+    author: (_, { id }) => find(authors, { id }),
+    quotes: () => quotes
+  },
+
+  Author: {
+    quotes: author => filter(quotes, {authorId: author.id}),
+  },
+
+  Quotes: {
+    author: quote => find(authors, {id: quote.authorId}),
   }
 };
 
-function createLambdaServer () {
+function createLambdaServer() {
   return new ApolloServerLambda({
     typeDefs,
     resolvers,
@@ -24,7 +60,7 @@ function createLambdaServer () {
   });
 }
 
-function createLocalServer () {
+function createLocalServer() {
   return new ApolloServer({
     typeDefs,
     resolvers,
